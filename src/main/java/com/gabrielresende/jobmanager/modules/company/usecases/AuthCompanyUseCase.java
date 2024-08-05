5be325bbbb0e9ea861e2467a625cd68fb8a348df
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2024-08-04 23:12:12 -0300

    auth

---
+++ src/main/java/com/gabrielresende/jobmanager/modules/company/usecases/AuthCompanyUseCase.java
@@ -0,0 +1,8 @@
+package com.gabrielresende.jobmanager.modules.company.usecases;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthCompanyUseCase {
+    
+}
--- src/main/java/com/gabrielresende/jobmanager/modules/company/usecases/CreateCompanyUseCase.java
+++ src/main/java/com/gabrielresende/jobmanager/modules/company/usecases/CreateCompanyUseCase.java
@@ -1,5 +1,6 @@
 package com.gabrielresende.jobmanager.modules.company.usecases;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.gabrielresende.jobmanager.exceptions.UserFoundException;
@@ -10,16 +11,23 @@ import com.gabrielresende.jobmanager.modules.company.repositories.CompanyReposit
 public class CreateCompanyUseCase {
 
     private final CompanyRepository companyRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public CreateCompanyUseCase(CompanyRepository companyRepository) {
+    public CreateCompanyUseCase(CompanyRepository companyRepository,
+            PasswordEncoder passwordEncoder) {
         this.companyRepository = companyRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void execute(CompanyEntity companyEntity) {
-        this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
+        this.companyRepository
+                .findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
                 .ifPresent(user -> {
                     throw new UserFoundException();
                 });
+
+        String password = this.passwordEncoder.encode(companyEntity.getPassword());
+        companyEntity.setPassword(password);
 
         this.companyRepository.save(companyEntity);
     }
